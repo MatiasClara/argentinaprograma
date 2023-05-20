97452c5d73f8879a0c6843a42eec4f224b7d7e74
MatiasClara <mc250604@ncr.com>
2023-05-20 10:43:12 -0300

    Agrego archivo

---
+++ hello-world.js
@@ -0,0 +1,12 @@
+let nombre = "Matias"
+console.log("Hello " + nombre);
+let num1 = 3;
+let num2 = 5;
+let suma = num1 + num2;
+let resta = num2 - num1;
+let multiplo = num1 * num2;
+let division = num2 / num1;
+console.log("La suma es" , suma);
+console.log("La resta es" , resta);
+console.log("La multiplicacion es" , multiplo);
+console.log("La division es" , division);
