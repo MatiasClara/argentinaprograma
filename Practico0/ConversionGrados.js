bd85747a0faf808bf05ffcd1e762ece479905d46
MatiasClara <mc250604@ncr.com>
2023-05-23 21:22:03 -0300

    Agrego el archivo conversionGrados.js

--- Practico0/ConversionGrados.js
+++ Practico0/ConversionGrados.js
@@ -1,3 +1,7 @@
-let gradosF = 80;
+let gradosF = 50;
 let gradosC = (gradosF - 32) * (5/9);
 console.log(gradosC);
+
+let gradosF2 = 75;
+let gradosC2 = (gradosF2 - 32) * (5/9);
+console.log(gradosC2);
