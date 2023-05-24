3daec0f6dbbf77b4bc6c837127b64bdfe52f69c1
MatiasClara <mc250604@ncr.com>
2023-05-23 21:20:59 -0300

    Practico N0 finalizado

--- Practico0/areaRectangulo.js
+++ Practico0/areaRectangulo.js
@@ -1,4 +1,9 @@
-let base = 12.5;
-let altura = 5.3;
+let base = 5;
+let altura = 3;
 let area = base * altura;
 console.log(area);
+
+let base2 = 12.5;
+let altura2 = 5.3;
+let area2 = base2 * altura2;
+console.log(area2);
--- Practico0/promedio3.js
+++ Practico0/promedio3.js
@@ -3,3 +3,15 @@ let num2 = 7;
 let num3 = 9;
 let promedio = (num1 + num2 + num3) / 3;
 console.log(promedio);
+
+let num4 = -2;
+let num5 = -3;
+let num6 = -8;
+let promedio2 = (num4 + num5 + num6) / 3;
+console.log(promedio2);
+
+let num7 = -1.2;
+let num8 = 5.3;
+let num9 = -3.3;
+let promedio3 = (num7 + num8 + num9) / 3;
+console.log(promedio3);
