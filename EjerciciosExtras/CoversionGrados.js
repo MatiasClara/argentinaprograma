121cbde4a75aa844ae2f4c782a6671c7c62e78c3
MatiasClara <mc250604@ncr.com>
2023-05-27 10:43:33 -0300

    Agrego carpeta de ejercicios extras

---
+++ EjerciciosExtras/CoversionGrados.js
@@ -0,0 +1,7 @@
+let gradosC = 10;
+let gradosF = (gradosC * 1.8) + 32;
+console.log(gradosF);
+
+let gradosC2 = 25;
+let gradosF2 = (gradosC2 * 1.8) + 32;
+console.log(gradosF2);
---
+++ EjerciciosExtras/Promedios.js
@@ -0,0 +1,3 @@
+let listaPromedios = [5, 7, 9, 11, 1, 9];
+let promedio = (listaPromedios[0] + listaPromedios[1] + listaPromedios[2] + listaPromedios[3] + listaPromedios[4] + listaPromedios[5]) / 6;
+console.log(promedio);
