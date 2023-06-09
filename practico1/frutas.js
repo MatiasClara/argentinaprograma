9cdae7564d23ee65bcad0b65a57d8008c231bb73
MatiasClara <mc250604@ncr.com>
2023-06-08 21:44:28 -0300

    Agrego archivos

---
+++ practico1/frutas.js
@@ -0,0 +1,9 @@
+let frutas = [];
+
+frutas.push("manzana", "naranja", "banana", "kiwi", "frutilla");
+
+let ultima = frutas[frutas.length - 1];
+console.log("La ultima fruta agregada fue: " + ultima);
+
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: " + primera);
--- practico1/operacionesAritmeticas.js
+++ practico1/operacionesAritmeticas.js
@@ -3,12 +3,12 @@ let x = 20;
 let y = 10;
 let z = 8;
 
-let wx = w + x;
-let yz = y - z;
-let resultadoFinal = wx * yz;
+let wMasX = w + x;
+let yMenosZ = y - z;
+let resultadoFinal = wMasX * yMenosZ;
 console.log(resultadoFinal);
 
 let esPar = (resultadoFinal % 2) === 0;
 console.log(esPar);
 
-console.log("Mis variables iniciales fueron: " +  w + ", " + x + ", " + y +  " y " + z + ". La respuesta a verificar si el resultado final es par es: " + esPar);
+console.log("Mis variables iniciales fueron: " + w + ", " + x + ", " + y +  " y " + z + ". La respuesta a verificar si el resultado final es par es: " + esPar);
