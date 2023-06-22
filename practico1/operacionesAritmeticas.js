28adf64a1245098798e1019f5be66845629d3aa6
MatiasClara <mc250604@ncr.com>
2023-06-22 18:48:24 -0300

    Corrijo segun la devolucion de Sofia

--- practico1/operacionesAritmeticas.js
+++ practico1/operacionesAritmeticas.js
@@ -6,9 +6,7 @@ let z = 8;
 let wMasX = w + x;
 let yMenosZ = y - z;
 let resultadoFinal = wMasX * yMenosZ;
-console.log(resultadoFinal);
 
 let esPar = (resultadoFinal % 2) === 0;
-console.log(esPar);
 
 console.log("Mis variables iniciales fueron: " + w + ", " + x + ", " + y +  " y " + z + ". La respuesta a verificar si el resultado final es par es: " + esPar);
