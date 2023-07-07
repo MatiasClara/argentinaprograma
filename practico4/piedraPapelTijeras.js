0ee608406ed124f5ff947de6cc99ee3772be79da
MatiasClara <mc250604@ncr.com>
2023-07-07 19:58:55 -0300

    Agrego piedrapapeltijeras

--- practico4/piedraPapelTijeras.js
+++ practico4/piedraPapelTijeras.js
@@ -1,29 +1,32 @@
+const readlineSync = require('readline-sync');
+const piedra = "piedra";
+const papel = "papel";
+const tijeras = "tijeras";
+
 function obtenerJugadaComputadora() {
     let numeroRandom = Math.floor(Math.random() * 3);
     let eleccionComputadora;
 switch (numeroRandom){
     case 0:
-        eleccionComputadora = "piedra";
+        eleccionComputadora = piedra;
         break;
     
     case 1:
-    eleccionComputadora = "papel";
+    eleccionComputadora = papel;
     break;
 
     case 2:
-        eleccionComputadora = "tijeras";
+        eleccionComputadora = tijeras;
 
     }
     return eleccionComputadora;
 }
 
 function obtenerJugadaUsuario(){
-    const readlineSync = require('readline-sync');
-    let eleccionUsuario = readlineSync.question("Elija: piedra, papel o tijeras");
+    let eleccionUsuario = readlineSync.question("Elija piedra, papel o tijeras: ");
     eleccionUsuario = eleccionUsuario.toLowerCase();
-    while (eleccionUsuario !== "piedra" && eleccionUsuario !== "papel" && eleccionUsuario !== "tijeras"){
-        const readlineSync = require('readline-sync');
-        let eleccionUsuario2 = readlineSync.question("Su ingreso es invalido, elija: piedra, papel o tijeras");
+    while (eleccionUsuario !== piedra && eleccionUsuario !== papel && eleccionUsuario !== tijeras){
+        let eleccionUsuario2 = readlineSync.question("Su ingreso es invalido, elija: piedra, papel o tijeras ");
         eleccionUsuario = eleccionUsuario2;
     }     
     return eleccionUsuario;
@@ -33,7 +36,7 @@ function determinarGanador(obtenerJugadaComputadora, obtenerJugadaUsuario) {
     let resultado;
     if (obtenerJugadaComputadora === obtenerJugadaUsuario){
         resultado = "Empate";
-    }else if ((obtenerJugadaComputadora === "piedra" && obtenerJugadaUsuario === "tijeras") || (obtenerJugadaComputadora === "papel" && obtenerJugadaUsuario === "piedra") || (obtenerJugadaComputadora === "tijeras" && obtenerJugadaUsuario === "papel")){
+    }else if ((obtenerJugadaComputadora === piedra && obtenerJugadaUsuario === tijeras) || (obtenerJugadaComputadora === papel && obtenerJugadaUsuario === piedra) || (obtenerJugadaComputadora === tijeras && obtenerJugadaUsuario === papel)){
         resultado = "Gana la computadora";
     }else{
         resultado = "Gana el usuario";
@@ -48,6 +51,4 @@ let ganador = determinarGanador(computadora, usuario);
 console.log("La computadora eligio: %s.", computadora);
 console.log("El usuario eligio: %s.", usuario);
 console.log("El resultado fue: %s.", ganador);
-
-
 
